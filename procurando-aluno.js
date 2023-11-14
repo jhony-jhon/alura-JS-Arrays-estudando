805af3bb07d936842290a6acbb94eaca74f6703d
Jhony Batista <telinabatista@gmail.com>
2023-11-13 22:46:34 -0300

    estudando o For()

---
+++ procurando-aluno.js
@@ -0,0 +1,29 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+function exibeNomeENota(aluno) {
+    if (listaDeAlunosEMedias[0].includes(aluno)) {
+        
+        // const alunos = listaDeAlunosEMedias[0];
+        // const medias = listaDeAlunosEMedias[1];
+
+        const [alunos, medias] = listaDeAlunosEMedias; //funciona como acima as const.
+        // essa sintaxe funciona como desestruturação de listas.
+
+        const indice = alunos.indexOf(aluno);
+        
+        const mediaDoAluno = medias[indice];
+        console.log(`${aluno} tem a média ${mediaDoAluno}.`);
+
+    } else {
+        console.log("Aluno não encontrado.");
+    }
+}
+
+// exibeNomeENota("Vitor");
+exibeNomeENota("Caio");
+
+
+
